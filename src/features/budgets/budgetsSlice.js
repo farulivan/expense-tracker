5a3b31324b35aba1deed1a9be790e8f89538a373
Farul Ivan <farulivan@gmail.com>
2022-10-10 13:11:33 +0700

    fix bug by rename a budgetsslice file

---
+++ src/features/budgets/budgetsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
+
+export const budgetsSlice = createSlice({
+  name: 'budgets',
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      state.map(budget => {
+        if (budget.category === action.payload.category) {
+          budget.amount = action.payload.amount
+        }
+        return budget;
+      });
+    }
+  } 
+})
+
+export const selectBudgets = (state) => state.budgets;
+export const { editBudget } = budgetsSlice.actions;
+export default budgetsSlice.reducer;
